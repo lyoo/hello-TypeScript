4ee1b5114e12b6c1b9f49415b7eafdfc97537d3f
lyoo <32499368@qq.com>
2020-05-01 23:18:14 +0800

    09 function void

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
-function add(a, b) {
-    return a + b;
-}
-var add2 = function (a, b) {
-    return a + b;
+// const add = (a: number, b: number): number => {
+//   return a + b;
+// };
+// const add = (a: number, b: number): string => {
+//   return a.toString() + b.toString();
+// };
+var add = function (a, b) {
+    console.log(a + b);
 };
 // const sum = add(10, 20);
-var sum = add2(10, 20);
+var sum = add(10, 20);
 console.log(sum);
--- index.ts
+++ index.ts
@@ -1,11 +1,15 @@
-function add(a: number, b: number) {
-  return a + b;
-}
+// const add = (a: number, b: number): number => {
+//   return a + b;
+// };
 
-const add2 = (a: number, b: number) => {
-  return a + b;
+// const add = (a: number, b: number): string => {
+//   return a.toString() + b.toString();
+// };
+
+const add = (a: number, b: number): void => {
+  console.log(a + b);
 };
 
 // const sum = add(10, 20);
-const sum = add2(10, 20);
+const sum = add(10, 20);
 console.log(sum);
