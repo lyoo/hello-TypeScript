ca795e88ab67ff807e21b8b8b5c593d3729a056d
lyoo <32499368@qq.com>
2020-05-01 22:57:24 +0800

    07 tuple

--- index.js
+++ index.js
@@ -1,13 +1,2 @@
-for (var i = 0; i <= 10; i++) {
-    console.log(i);
-}
-// 定义类型和复制
-// let my_name: string = "marsliang";
-// var my_name: string = "marsliang";
-var my_name = "marsliang";
-console.log("my name is " + my_name);
-// 无法重新声明块范围变量“my_name”
-// let my_name: string = "lyoo";
-// var my_name: number = 111;
-my_name = "lyoo";
-console.log("my name is \" + " + my_name);
+// tuple
+var my_tuple = ["lyoo", 18];
