a3e97761ede5d2bfae24e68c086005d3e7f445d8
lyoo <32499368@qq.com>
2020-05-01 22:16:45 +0800

    04变量声明

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-var fn = function () { return "response"; };
+for (var i = 0; i <= 10; i++) {
+    console.log(i);
+}
+// 定义类型和复制
+// let my_name: string = "marsliang";
+// var my_name: string = "marsliang";
+var my_name = "marsliang";
+console.log("my name is " + my_name);
+// 无法重新声明块范围变量“my_name”
+// let my_name: string = "lyoo";
+// var my_name: number = 111;
+my_name = "lyoo";
+console.log("my name is \" + " + my_name);
--- index.ts
+++ index.ts
@@ -1,1 +1,15 @@
-const fn = () => "response";
+for (let i = 0; i <= 10; i++) {
+  console.log(i);
+}
+
+// 定义类型和复制
+// let my_name: string = "marsliang";
+// var my_name: string = "marsliang";
+let my_name: string = "marsliang";
+console.log("my name is " + my_name);
+
+// 无法重新声明块范围变量“my_name”
+// let my_name: string = "lyoo";
+// var my_name: number = 111;
+my_name = "lyoo";
+console.log(`my name is " + ${my_name}`);
