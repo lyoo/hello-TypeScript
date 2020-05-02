1c0b1e6d8067d6ac9e607fee6284901193d9c255
lyoo <32499368@qq.com>
2020-05-02 15:26:23 +0800

    17 member visibility

--- index.js
+++ index.js
@@ -1,51 +1,30 @@
 var Person = /** @class */ (function () {
     function Person() {
     }
+    Person.prototype.greet = function () {
+        console.log("hi");
+    };
+    Person.prototype.sayHi = function () {
+        console.log("private say hi");
+    };
+    Person.prototype.callSayHi = function () {
+        this.sayHi();
+        console.log("******");
+    };
+    Person.prototype.getFirstName = function () {
+        console.log(this.firstName);
+    };
+    Person.prototype.setFirstName = function (firstName) {
+        this.firstName = firstName;
+    };
+    Person.prototype.othergreet = function () {
+        this.greet();
+        console.log("******");
+    };
     return Person;
 }());
-// 生成一个对象
 var aPerson = new Person();
-// 设置 firstName 的内容
-aPerson.firstName = "marsliang";
-// aPerson.firstName = 10;
-// 读取 firstName 的内容
-console.log(aPerson.firstName);
-var Movie = /** @class */ (function () {
-    // this 指向生成的 object 本身
-    function Movie(name, play_count, time) {
-        this.name = name;
-        this.play_count = play_count;
-        this.time = time;
-    }
-    // method 方法 可能对数据进行操作
-    Movie.prototype.display_play_count = function (padding) {
-        if (padding === void 0) { padding = "******"; }
-        return this.play_count + " \u6B21\u6570 " + padding;
-    };
-    Movie.prototype.increase_play_count = function () {
-        this.play_count += 1;
-    };
-    return Movie;
-}());
-// 生成一个对象
-var m = new Movie("TypeScript 教程", 15, 80);
-console.log(m.name);
-console.log("\u64AD\u653E\u65F6\u957F" + m.time + "\u5206\u949F");
-console.log(m.display_play_count());
-m.increase_play_count();
-console.log(m.display_play_count());
-m.increase_play_count();
-console.log(m.display_play_count());
-m.increase_play_count();
-console.log(m.display_play_count());
-// 生成一个对象
-var m1 = new Movie("Nodejs 教程", 183, 20);
-console.log(m1.name);
-console.log("\u64AD\u653E\u65F6\u957F" + m1.time + "\u5206\u949F");
-console.log(m1.display_play_count());
-m1.increase_play_count();
-console.log(m1.display_play_count());
-m1.increase_play_count();
-console.log(m1.display_play_count());
-m1.increase_play_count();
-console.log(m1.display_play_count());
+// console.log(aPerson.firstName);
+aPerson.setFirstName("marsliang");
+aPerson.getFirstName();
+aPerson.callSayHi();
--- index.ts
+++ index.ts
@@ -1,63 +1,38 @@
 class Person {
   // 定义2个数据内容
-  firstName: string;
-  lastName: string;
+  private firstName: string;
+  private lastName: string;
+
+  public greet() {
+    console.log("hi");
+  }
+
+  private sayHi() {
+    console.log("private say hi");
+  }
+
+  public callSayHi() {
+    this.sayHi();
+    console.log("******");
+  }
+
+  getFirstName() {
+    console.log(this.firstName);
+  }
+
+  setFirstName(firstName: string) {
+    this.firstName = firstName;
+  }
+
+  public othergreet() {
+    this.greet();
+    console.log("******");
+  }
 }
 
-// 生成一个对象
 let aPerson = new Person();
+// console.log(aPerson.firstName);
+aPerson.setFirstName("marsliang");
+aPerson.getFirstName();
 
-// 设置 firstName 的内容
-aPerson.firstName = "marsliang";
-// aPerson.firstName = 10;
-
-// 读取 firstName 的内容
-console.log(aPerson.firstName);
-
-class Movie {
-  name: string;
-  play_count: number;
-  created_at: string;
-  time: number;
-
-  // this 指向生成的 object 本身
-  constructor(name: string, play_count: number, time: number) {
-    this.name = name;
-    this.play_count = play_count;
-    this.time = time;
-  }
-
-  // method 方法 可能对数据进行操作
-  display_play_count(padding: string = "******") {
-    return `${this.play_count} 次数 ${padding}`;
-  }
-
-  increase_play_count() {
-    this.play_count += 1;
-  }
-}
-
-// 生成一个对象
-let m = new Movie("TypeScript 教程", 15, 80);
-console.log(m.name);
-console.log(`播放时长${m.time}分钟`);
-console.log(m.display_play_count());
-m.increase_play_count();
-console.log(m.display_play_count());
-m.increase_play_count();
-console.log(m.display_play_count());
-m.increase_play_count();
-console.log(m.display_play_count());
-
-// 生成一个对象
-let m1 = new Movie("Nodejs 教程", 183, 20);
-
-console.log(m1.name);
-console.log(`播放时长${m1.time}分钟`);
-console.log(m1.display_play_count());
-m1.increase_play_count();
-console.log(m1.display_play_count());
-m1.increase_play_count();
-console.log(m1.display_play_count());
-m1.increase_play_count();
-console.log(m1.display_play_count());
+aPerson.callSayHi();
