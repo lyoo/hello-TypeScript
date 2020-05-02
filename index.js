e02808f91eb32a162b197addfdce88c1301d1f18
lyoo <32499368@qq.com>
2020-05-02 22:14:17 +0800

    25 use interface

--- index.js
+++ index.js
@@ -1,20 +1,20 @@
-// 枚举类型
-// 它的值是数字序号，从0开始
-// 代码可读性强
-// 可能会常用下拉框等应用
-var DaysOfTheWeek;
-(function (DaysOfTheWeek) {
-    DaysOfTheWeek[DaysOfTheWeek["SUN"] = 100] = "SUN";
-    DaysOfTheWeek[DaysOfTheWeek["MON"] = 101] = "MON";
-    DaysOfTheWeek[DaysOfTheWeek["TUE"] = 102] = "TUE";
-    DaysOfTheWeek[DaysOfTheWeek["WED"] = 103] = "WED";
-    DaysOfTheWeek[DaysOfTheWeek["THU"] = 104] = "THU";
-    DaysOfTheWeek[DaysOfTheWeek["FRI"] = 105] = "FRI";
-    DaysOfTheWeek[DaysOfTheWeek["SAT"] = 106] = "SAT";
-})(DaysOfTheWeek || (DaysOfTheWeek = {}));
-var day;
-day = DaysOfTheWeek.MON;
-if (day === DaysOfTheWeek.MON) {
-    console.log("Got to go to work");
-}
-console.log(day);
+// 只要传入参数o 有 name  属性
+// o 可以是一个接口类型，这个接口类型要有 name 属性
+var sayName = function (o) {
+    console.log(o.name);
+};
+var person = {
+    // name: "lyoo",
+    age: 18
+};
+sayName(person);
+// const bottle = {
+//   name: "漂流瓶",
+//   litres: 1,
+// };
+// class Person {
+//   name: string;
+// }
+// let aPerson = new Person();
+// aPerson.name = "marsliang";
+// sayName(bottle);
--- index.ts
+++ index.ts
@@ -1,6 +1,12 @@
+interface Named {
+  // 属性
+  name: string;
+}
+
 // 只要传入参数o 有 name  属性
 // o 可以是一个接口类型，这个接口类型要有 name 属性
-const sayName = (o) => {
+// 定义 o 为接口类型, 可以传入一个对象
+const sayName = (o: Named) => {
   console.log(o.name);
 };
 
@@ -9,10 +15,12 @@ const person = {
   age: 18,
 };
 
-const bottle = {
-  name: "漂流瓶",
-  litres: 1,
-};
+sayName(person);
+
+// const bottle = {
+//   name: "漂流瓶",
+//   litres: 1,
+// };
 
 class Person {
   name: string;
@@ -21,5 +29,6 @@ class Person {
 let aPerson = new Person();
 aPerson.name = "marsliang";
 
-sayName(person);
-sayName(bottle);
+sayName(aPerson);
+
+// sayName(bottle);
