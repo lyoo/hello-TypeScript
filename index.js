faed76734e5d596bad76fec2e1937c78f8c8b513
lyoo <32499368@qq.com>
2020-05-02 10:35:14 +0800

    15  constructor methods

--- index.js
+++ index.js
@@ -11,8 +11,41 @@ aPerson.firstName = "marsliang";
 // 读取 firstName 的内容
 console.log(aPerson.firstName);
 var Movie = /** @class */ (function () {
-    function Movie() {
+    // this 指向生成的 object 本身
+    function Movie(name, play_count, time) {
+        this.name = name;
+        this.play_count = play_count;
+        this.time = time;
     }
+    // method 方法 可能对数据进行操作
+    Movie.prototype.display_play_count = function (padding) {
+        if (padding === void 0) { padding = "******"; }
+        return this.play_count + " \u6B21\u6570 " + padding;
+    };
+    Movie.prototype.increase_play_count = function () {
+        this.play_count += 1;
+    };
     return Movie;
 }());
-var m = new Movie();
+// 生成一个对象
+var m = new Movie("TypeScript 教程", 15, 80);
+console.log(m.name);
+console.log("\u64AD\u653E\u65F6\u957F" + m.time + "\u5206\u949F");
+console.log(m.display_play_count());
+m.increase_play_count();
+console.log(m.display_play_count());
+m.increase_play_count();
+console.log(m.display_play_count());
+m.increase_play_count();
+console.log(m.display_play_count());
+// 生成一个对象
+var m1 = new Movie("Nodejs 教程", 183, 20);
+console.log(m1.name);
+console.log("\u64AD\u653E\u65F6\u957F" + m1.time + "\u5206\u949F");
+console.log(m1.display_play_count());
+m1.increase_play_count();
+console.log(m1.display_play_count());
+m1.increase_play_count();
+console.log(m1.display_play_count());
+m1.increase_play_count();
+console.log(m1.display_play_count());
--- index.ts
+++ index.ts
@@ -18,7 +18,46 @@ class Movie {
   name: string;
   play_count: number;
   created_at: string;
-  time: string;
+  time: number;
+
+  // this 指向生成的 object 本身
+  constructor(name: string, play_count: number, time: number) {
+    this.name = name;
+    this.play_count = play_count;
+    this.time = time;
+  }
+
+  // method 方法 可能对数据进行操作
+  display_play_count(padding: string = "******") {
+    return `${this.play_count} 次数 ${padding}`;
+  }
+
+  increase_play_count() {
+    this.play_count += 1;
+  }
 }
 
-let m = new Movie();
+// 生成一个对象
+let m = new Movie("TypeScript 教程", 15, 80);
+console.log(m.name);
+console.log(`播放时长${m.time}分钟`);
+console.log(m.display_play_count());
+m.increase_play_count();
+console.log(m.display_play_count());
+m.increase_play_count();
+console.log(m.display_play_count());
+m.increase_play_count();
+console.log(m.display_play_count());
+
+// 生成一个对象
+let m1 = new Movie("Nodejs 教程", 183, 20);
+
+console.log(m1.name);
+console.log(`播放时长${m1.time}分钟`);
+console.log(m1.display_play_count());
+m1.increase_play_count();
+console.log(m1.display_play_count());
+m1.increase_play_count();
+console.log(m1.display_play_count());
+m1.increase_play_count();
+console.log(m1.display_play_count());
