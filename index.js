9f562ca82108359e5c145537e8306bbff1261046
lyoo <32499368@qq.com>
2020-05-02 09:52:37 +0800

    13 union type

--- index.js
+++ index.js
@@ -1,20 +1,29 @@
-var a;
-a = 10;
-a = "lyoo";
-a = [1, 2, 3];
+function isNumber(value) {
+    // 可以进行进一步判断
+    return typeof value === "number";
+}
+function isString(value) {
+    return typeof value === "string";
+}
+// const log = (value: any) => {
+//   console.log(typeof value);
+//   if (isNumber(value)) {
+//     return "your number is " + value;
+//   }
+//   if (typeof value === "string") {
+//     return "your string is " + value;
+//   }
+//   throw new Error("Expected string or number, got " + value);
+// };
 var log = function (value) {
     console.log(typeof value);
-    if (typeof value === "number") {
+    if (isNumber(value)) {
         return "your number is " + value;
     }
     if (typeof value === "string") {
         return "your string is " + value;
     }
-    throw new Error("Expected string or number, got " + value);
 };
 console.log(log("marsliang"));
 console.log(log(12));
-console.log(log([1, 2, 3]));
-var b;
-// b = "10";
-b = [1, "2", [1]];
+// console.log(log([1, 2, 3]));
--- index.ts
+++ index.ts
@@ -1,26 +1,32 @@
-let a: any;
+function isNumber(value: any): value is number {
+  // 可以进行进一步判断
+  return typeof value === "number";
+}
 
-a = 10;
-a = "lyoo";
-a = [1, 2, 3];
+function isString(value: any): value is string {
+  return typeof value === "string";
+}
 
-const log = (value: any) => {
+// const log = (value: any) => {
+//   console.log(typeof value);
+//   if (isNumber(value)) {
+//     return "your number is " + value;
+//   }
+//   if (typeof value === "string") {
+//     return "your string is " + value;
+//   }
+//   throw new Error("Expected string or number, got " + value);
+// };
+
+const log = (value: string | number | null) => {
   console.log(typeof value);
-  if (typeof value === "number") {
-    return `your number is ${value}`;
+  if (isNumber(value)) {
+    return "your number is " + value;
   }
-
   if (typeof value === "string") {
-    return `your string is ${value}`;
+    return "your string is " + value;
   }
-
-  throw new Error(`Expected string or number, got ${value}`);
 };
-
 console.log(log("marsliang"));
 console.log(log(12));
-console.log(log([1, 2, 3]));
-
-let b: any[];
-// b = "10";
-b = [1, "2", [1]];
+// console.log(log([1, 2, 3]));
