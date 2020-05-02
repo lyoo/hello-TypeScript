6b753b7f6fe755acfa00092cbccddc012967811b
lyoo <32499368@qq.com>
2020-05-02 08:45:01 +0800

    10 function default arguments

--- index.js
+++ index.js
@@ -5,8 +5,13 @@
 //   return a.toString() + b.toString();
 // };
 var add = function (a, b) {
-    console.log(a + b);
+    if (b) {
+        console.log(a + b);
+    }
+    else {
+        console.log(a);
+    }
 };
 // const sum = add(10, 20);
-var sum = add(10, 20);
+var sum = add(10, 30);
 console.log(sum);
--- index.ts
+++ index.ts
@@ -6,10 +6,14 @@
 //   return a.toString() + b.toString();
 // };
 
-const add = (a: number, b: number): void => {
-  console.log(a + b);
+const add = (a: number, b?: number): void => {
+  if (b) {
+    console.log(a + b);
+  } else {
+    console.log(a);
+  }
 };
 
 // const sum = add(10, 20);
-const sum = add(10, 20);
+const sum = add(10, 30);
 console.log(sum);
