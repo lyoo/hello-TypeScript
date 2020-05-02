4d15ab56dec7a2e1f86b7c3c4c7adaffd8a576ab
lyoo <32499368@qq.com>
2020-05-02 17:25:16 +0800

    22 enum

--- index.js
+++ index.js
@@ -1,89 +1,20 @@
-var Person = /** @class */ (function () {
-    // static all() {
-    //  // Person 对应数据库的一个表模型
-    //  // 可以列出 persons 表的所有记录
-    //  // new 出来的可能是一条记录，要找到所有记录，可能要通过静态方法
-    //  return Person.select().all();
-    // }
-    function Person(firstName, lastName) {
-        // 只读属性 不能修改
-        this.name = "marsliang";
-        this.firstName = firstName;
-        this.lastName = lastName;
-    }
-    // 静态方法
-    Person.getStaticAge = function () {
-        return "my age is " + Person.age;
-    };
-    Person.prototype.getAge = function () {
-        return "my age is " + Person.age;
-    };
-    Person.prototype.greet = function () {
-        console.log("hi");
-    };
-    Person.prototype.sayHi = function () {
-        console.log("private say hi");
-    };
-    Person.prototype.callSayHi = function () {
-        this.sayHi();
-        console.log("******");
-    };
-    Person.prototype.getFirstName = function () {
-        return this.firstName;
-    };
-    Person.prototype.getLastName = function () {
-        return this.lastName;
-    };
-    Person.prototype.setFirstName = function (firstName) {
-        this.firstName = firstName;
-    };
-    Person.prototype.setLastName = function (lastName) {
-        this.lastName = lastName;
-    };
-    Person.prototype.othergreet = function () {
-        this.greet();
-        console.log("******");
-    };
-    // 静态属性
-    Person.age = 10;
-    return Person;
-}());
-// class Programmer extends Person {
-//   constructor(firstName: string, lastName: string) {
-//     // 调用父类的构造函数的方法，要传入参数
-//     super(firstName, lastName);
-//     console.log("Programmer constructor");
-//   }
-//   public static getSuperAge() {
-//     return `super age is ${Programmer.age}`;
-//   }
-//   public greet() {
-//     console.log("hello world");
-//   }
-//   public greetLikeNormalPeople() {
-//     super.greet();
-//   }
-//   // public getFullName(): string {
-//   //   return `${this.firstName} ${this.lastName}`;
-//   // }
-//   getSuperFirstName(): string {
-//     return this.getFirstName();
-//   }
-//   getSuperLastName(): string {
-//     return this.lastName;
-//   }
-//   // getFirstName 和 getLastName 继承而来
-//   // public getFullName(): string {
-//   //   return `${this.getFirstName()} ${this.getLastName()}`;
-//   // }
-//   public getFullName(): string {
-//     return `${this.getSuperFirstName()} ${this.getSuperLastName()}`;
-//   }
-// }
-var aPerson = new Person("marsliang", "lyoo");
-aPerson.name = "111";
-console.log(aPerson.name);
-// let aProgrammer = new Programmer("marsliang", "lyoo");
-// console.log(aPerson.getAge());
-// console.log(Person.getStaticAge());
-// console.log(Programmer.getSuperAge());
+// 枚举类型
+// 它的值是数字序号，从0开始
+// 代码可读性强
+// 可能会常用下拉框等应用
+var DaysOfTheWeek;
+(function (DaysOfTheWeek) {
+    DaysOfTheWeek[DaysOfTheWeek["SUN"] = 0] = "SUN";
+    DaysOfTheWeek[DaysOfTheWeek["MON"] = 1] = "MON";
+    DaysOfTheWeek[DaysOfTheWeek["TUE"] = 2] = "TUE";
+    DaysOfTheWeek[DaysOfTheWeek["WED"] = 3] = "WED";
+    DaysOfTheWeek[DaysOfTheWeek["THU"] = 4] = "THU";
+    DaysOfTheWeek[DaysOfTheWeek["FRI"] = 5] = "FRI";
+    DaysOfTheWeek[DaysOfTheWeek["SAT"] = 6] = "SAT";
+})(DaysOfTheWeek || (DaysOfTheWeek = {}));
+var day;
+day = DaysOfTheWeek.MON;
+if (day === DaysOfTheWeek.MON) {
+    console.log("Got to go to work");
+}
+console.log(day);
--- index.ts
+++ index.ts
@@ -1,115 +1,23 @@
-class Person {
-  // 定义2个数据内容
-  protected firstName: string;
-  protected lastName: string;
-
-  // 只读属性 不能修改
-  readonly name: string = "marsliang";
-
-  // 静态属性
-  protected static age: number = 10;
-
-  // 静态方法
-  public static getStaticAge() {
-    return `my age is ${Person.age}`;
-  }
-
-  // static all() {
-  //  // Person 对应数据库的一个表模型
-  //  // 可以列出 persons 表的所有记录
-  //  // new 出来的可能是一条记录，要找到所有记录，可能要通过静态方法
-  //  return Person.select().all();
-  // }
-
-  public constructor(firstName: string, lastName: string) {
-    this.firstName = firstName;
-    this.lastName = lastName;
-  }
-
-  getAge() {
-    return `my age is ${Person.age}`;
-  }
-
-  public greet() {
-    console.log("hi");
-  }
-
-  private sayHi() {
-    console.log("private say hi");
-  }
-
-  public callSayHi() {
-    this.sayHi();
-    console.log("******");
-  }
-
-  protected getFirstName() {
-    return this.firstName;
-  }
-
-  getLastName() {
-    return this.lastName;
-  }
-
-  setFirstName(firstName: string) {
-    this.firstName = firstName;
-  }
-
-  setLastName(lastName: string) {
-    this.lastName = lastName;
-  }
-
-  public othergreet() {
-    this.greet();
-    console.log("******");
-  }
+// 枚举类型
+// 它的值是数字序号，从0开始
+// 代码可读性强
+// 可能会常用下拉框等应用
+enum DaysOfTheWeek {
+  SUN,
+  MON,
+  TUE,
+  WED,
+  THU,
+  FRI,
+  SAT,
 }
 
-// class Programmer extends Person {
-//   constructor(firstName: string, lastName: string) {
-//     // 调用父类的构造函数的方法，要传入参数
-//     super(firstName, lastName);
-//     console.log("Programmer constructor");
-//   }
+let day: DaysOfTheWeek;
 
-//   public static getSuperAge() {
-//     return `super age is ${Programmer.age}`;
-//   }
+day = DaysOfTheWeek.MON;
 
-//   public greet() {
-//     console.log("hello world");
-//   }
+if (day === DaysOfTheWeek.MON) {
+  console.log("Got to go to work");
+}
 
-//   public greetLikeNormalPeople() {
-//     super.greet();
-//   }
-
-//   // public getFullName(): string {
-//   //   return `${this.firstName} ${this.lastName}`;
-//   // }
-
-//   getSuperFirstName(): string {
-//     return this.getFirstName();
-//   }
-
-//   getSuperLastName(): string {
-//     return this.lastName;
-//   }
-
-//   // getFirstName 和 getLastName 继承而来
-//   // public getFullName(): string {
-//   //   return `${this.getFirstName()} ${this.getLastName()}`;
-//   // }
-
-//   public getFullName(): string {
-//     return `${this.getSuperFirstName()} ${this.getSuperLastName()}`;
-//   }
-// }
-
-let aPerson = new Person("marsliang", "lyoo");
-aPerson.name = "111";
-console.log(aPerson.name);
-// let aProgrammer = new Programmer("marsliang", "lyoo");
-// console.log(aPerson.getAge());
-// console.log(Person.getStaticAge());
-// console.log(Programmer.getSuperAge());
+console.log(day);
