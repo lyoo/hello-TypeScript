6687380bbdd1e38370ca8dd242713c1cd29007f9
lyoo <32499368@qq.com>
2020-05-03 01:08:55 +0800

    29 excess property checks

--- index.ts
+++ index.ts
@@ -1,74 +1,29 @@
-// 接口
 interface Person {
-  name: string;
+  firstName: string;
+  lastName?: string;
 
-  greet(): void;
+  print?(): void;
 }
+
+let person = {
+  firstName: "marsliang",
+  // lastName: "lyoo",
+};
+
+class Programmer implements Person {
+  firstName: string;
+}
+
+const programmer = new Programmer();
+programmer.firstName = "mars";
 
 const sayName = (o: Person) => {
-  o.greet();
+  console.log(o.firstName);
 };
 
-// 实现接口 微信支付 支付宝支付
+sayName(person);
 
-// 类实现接口
-class Employee implements Person {
-  name: string;
+sayName(programmer);
 
-  greet(): void {
-    console.log("I am employee");
-  }
-}
-
-class Customer implements Person {
-  public name: string;
-  public email: string;
-  greet(): void {
-    console.log("I am customer");
-  }
-}
-
-// let cu = new Customer();
-// cu.greet();
-// sayName(cu);
-
-// let em = new Employee();
-// em.greet();
-// sayName(em);
-
-let customer: Person = new Customer();
-customer.greet();
-
-let employee: Person = new Employee();
-employee.greet();
-
-// 支付接口
-interface Pay {
-  post(): void;
-}
-
-// 可能会发送 http 请求
-// 真正支付的请求
-const doPay = (pay: Pay) => {
-  // 有一些逻辑
-  pay.post();
-};
-
-// 微信支付
-class WePay implements Pay {
-  // 微信支付宝支付接口
-  post() {}
-}
-
-// 支付宝支付
-class AliPay implements Pay {
-  post() {}
-}
-
-// 其他接口
-
-let wePay: Pay = new WePay();
-let aliPay: Pay = new AliPay();
-
-doPay(wePay);
-doPay(aliPay);
+// 类型断言
+sayName({ firstName: "a", lastName: "b", age: 18 } as Person);
