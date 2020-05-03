bdff2d9883f8e8b23ff41cb649810902a4ba76f9
lyoo <32499368@qq.com>
2020-05-03 23:22:43 +0800

    41 class accessors get set

--- index.ts
+++ index.ts
@@ -1,16 +1,38 @@
-// class Person {
-//   private _name: string;
-//   private _age: number;
-
-//   constructor(name: string, age: number) {
-//     this._name = name;
-//     this._age = age;
-//   }
-// }
-
 class Person {
-  constructor(private _name: string, private _age: number) {}
+  private _name: string;
+  private _age: number;
+
+  constructor(name: string, age: number) {
+    this._name = name;
+    this._age = age;
+  }
+
+  // 读取 name
+  getName(): string {
+    return this._name;
+  }
+
+  // 设置 name
+  setName(name: string): void {
+    this._name = name;
+  }
+
+  get name(): string {
+    return this._name;
+  }
+
+  // Accessors are only available when targeting ECMAScript 5 and higher.
+  set name(name: string) {
+    this._name = name;
+  }
 }
 
 let p: Person = new Person("lyoo", 18);
-console.log("p: ", p);
+// 输出 name
+console.log(p.name);
+// console.log(p.getName());
+
+// 修改 name
+p.name = "marsliang";
+// p.setName("marsliang");
+console.log(p);
