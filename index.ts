c4234806a6f985609201b874688b13af0ea2866b
lyoo <32499368@qq.com>
2020-05-03 21:51:33 +0800

    37 indexable type 2

--- index.ts
+++ index.ts
@@ -4,6 +4,7 @@ interface States {
 
 interface States1 {
   // An index signature parameter type must be 'string' or 'number'.
+  // 这种方式定义的数组没有 push length join 等属性和方法
   [index: number]: number;
 }
 
@@ -15,10 +16,58 @@ console.log(s);
 console.log(s["enabled"]);
 
 let s1: States1 = [1, 2, 3, 4];
-console.log(s1);
-console.log(s1[0]);
+// console.log(s1);
+// console.log(s1[0]);
+// 类型“States1”上不存在属性“join”。
+// console.log(s1.join("-"));
 // Property 'length' does not exist on type 'States1'.
 // console.log(s1.length);
 
 let s2: number[] = [1, 2, 3];
 console.log(s2.length);
+console.log(s2.join("-"));
+
+interface States2 {
+  [index: number]: boolean;
+  length: number;
+
+  pop(): boolean;
+  push(): void;
+}
+
+let s3: States2 = [true, false, false, true];
+console.log(s3);
+console.log(s3.length);
+console.log(s3.pop());
+
+interface States3 {
+  [state: string]: boolean;
+  mainScreen: boolean;
+}
+
+let s4: States3 = {
+  mainScreen: true,
+};
+s4["enabled"] = false;
+
+interface NestedCss {
+  color?: string;
+  nest?: {
+    [selector: string]: NestedCss;
+  };
+}
+
+let example: NestedCss = {
+  color: "red",
+  nest: {
+    ".subclass": {
+      color: "blue",
+    },
+  },
+};
+
+let c1: NestedCss = {
+  color: "red",
+};
+
+console.log("c1: ", c1);
