531ff24f22de1082185389de0c03be4b0e1c710a
lyoo <32499368@qq.com>
2020-05-03 22:47:57 +0800

    39 abstract class

--- index.ts
+++ index.ts
@@ -1,41 +1,35 @@
-interface Todo {
-  userId: number;
-  id: number;
-  title: string;
-  completed: boolean;
+// 不能实例化
+abstract class Person {
+  abstract name: string;
+
+  // constructor(name: string) {
+  //   this.name = name;
+  // }
+
+  display(): void {
+    console.log("name: ", this.name);
+  }
+
+  // 抽象方法，没有方法体
+  abstract find(string): Person;
+}
+// 非抽象类“Employee”不会实现继承自“Person”类的抽象成员“find”。
+class Employee extends Person {
+  name: string;
+  empCode: number;
+
+  constructor(name: string, code: number) {
+    super();
+    this.name = name;
+    this.empCode = code;
+  }
+
+  // 必须实现抽象的方法
+  find(name: string): Person {
+    return new Employee(name, 1);
+  }
 }
 
-class Todo1 {
-  userId: number;
-  id: number;
-  title: string;
-  completed: boolean;
-}
-
-let todo: Todo = {
-  userId: 1,
-  id: 1,
-  title: "delectus aut autem",
-  completed: false,
-};
-
-let todos: Todo1[] = [
-  {
-    userId: 1,
-    id: 1,
-    title: "delectus aut autem",
-    completed: false,
-  },
-  {
-    userId: 1,
-    id: 2,
-    title: "quis ut nam facilis et officia qui",
-    completed: false,
-  },
-  {
-    userId: 1,
-    id: 3,
-    title: "fugiat veniam minus",
-    completed: false,
-  },
-];
+let p: Person = new Employee("xxx", 12);
+let p1: Person = p.find("lyoo");
+console.log(p1);
