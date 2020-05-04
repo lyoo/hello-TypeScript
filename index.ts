c14326a528b59eeac25259edc67f3deb2c8863bc
lyoo <32499368@qq.com>
2020-05-05 01:48:32 +0800

    55 generics class

--- index.ts
+++ index.ts
@@ -1,65 +1,56 @@
-// function getArray(items: any[]): any[] {
-//   return new Array().concat(items);
-// }
+class List<T> {
+  private data: T[];
 
-// let myNumArray = getArray([100, 200, 300]);
-// let myStrArray = getArray(["hello", "world"]);
+  constructor(elements: T[]) {
+    this.data = elements;
+  }
 
-// console.log(myNumArray);
-// console.log(myStrArray);
+  add(t: T) {
+    this.data.push(t);
+  }
 
-// myNumArray.push(400);
-// myStrArray.push("lyoo");
+  remove(t: T) {
+    let index = this.data.indexOf(t);
+    if (index > -1) {
+      this.data.splice(index, 1);
+    }
+  }
 
-// console.log(myNumArray);
-// console.log(myStrArray);
-
-// myNumArray.push("marsliang");
-// myStrArray.push(500);
-
-// console.log(myNumArray);
-// console.log(myStrArray);
-
-// 限制所有的元素是纯数字或者字符串
-// function getArray(items: number[]): number[] {
-//   return new Array().concat(items);
-// }
-
-// function getArray(items: string[]): string[] {
-//   return new Array().concat(items);
-// }
-
-// 可以用很多类型来代替 T
-// T[] 代表数组，每个元素都是 T 类型
-function getArray<T>(items: T[]): T[] {
-  return new Array<T>().concat(items);
+  asArray(): T[] {
+    return this.data;
+  }
 }
 
-// 调用，可以传入很多类型，代码复用
-let myNumArray = getArray<number>([100, 200, 300]);
-let myStrArray = getArray<string>(["helo", "world"]);
-let myBooleanArray = getArray([true, false]);
+let numbers = new List<number>([1, 2, 3, 4]);
+numbers.add(5);
+numbers.remove(3);
+let numArray = numbers.asArray();
+console.log(numbers);
 
-myNumArray.push(400);
-myStrArray.push("lyoo");
-// 推断出 boolean, 不推荐使用 类型“400”的参数不能赋给类型“boolean”的参数。
-// myBooleanArray.push(400);
+let fruits = new List<string>(["apple", "banana", "orange"]);
+fruits.add("mango");
+fruits.remove("banana");
+let fruitArray = fruits.asArray();
+console.log(fruitArray);
 
-console.log(myNumArray);
-console.log(myStrArray);
+class Pair<F, S> {
+  private _first: F;
+  private _second: S;
 
-// function getArray1<T>(items: T): T {
-//   console.log(items.length);
-//   return T;
-// }
+  constructor(first: F, second: S) {
+    this._first = first;
+    this._second = second;
+  }
 
-function getArray1<T>(items: T[]): T[] {
-  console.log(items.length);
-  return items;
+  get first(): F {
+    return this._first;
+  }
+
+  get second(): S {
+    return this._second;
+  }
 }
 
-// myNumArray.push("marsliang");
-// myStrArray.push(500);
-
-console.log(myNumArray);
-console.log(myStrArray);
+let pair = new Pair<boolean, string>(true, "111");
+console.log(pair.first);
+console.log(pair.second);
