b3c2beae48d5fc6806c700bb17ab3292628eb88f
lyoo <32499368@qq.com>
2020-05-04 16:05:51 +0800

    49 non null assertion operator

--- index.ts
+++ index.ts
@@ -1,18 +1,14 @@
-// x? number | undefined
-function show(x?: number | null): void {
-  if (x === undefined) {
-    console.log("value not set");
-  } else if (x === null) {
-    console.log("value is null");
-  } else {
-    console.log(x);
-  }
+function splitInHalf(str: string | null): string {
+  let checkString = function () {
+    if (str === null || str === undefined) {
+      str = "test";
+    }
+  };
+  checkString();
+  console.log(str!.length / 2);
+  // 告诉编辑器 str 不能为空
+  return str!.substring(0, str!.length / 2);
 }
 
-let x = 10;
-let y;
-let z = null;
-
-show(x);
-show(y);
-show(z);
+let s: string = splitInHalf("hello");
+console.log(s);
