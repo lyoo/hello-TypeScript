640daa11531bce2ba00c61ac7f3c99a7a75594f4
lyoo <32499368@qq.com>
2020-05-04 10:19:06 +0800

    44 function overloading

--- index.ts
+++ index.ts
@@ -1,54 +1,59 @@
-// 第一种 不被推荐 any
-let a: any;
+// 函数名相同 参数不同 参数类型相同
+// 没有实现定义
+function sum(x: number, y: number): number;
+function sum(x: number, y: number, z: number): number;
 
-a = function (): void {
-  console.log("It work");
-};
-
-// 返回匿名函数
-function fun(): any {
-  return function (): void {
-    console.log("It fun");
-  };
+// 上面两种定义的组合实现
+function sum(x: number, y: number, z?: number): number {
+  console.log(typeof z);
+  if (typeof z === "undefined") {
+    return x + y;
+  } else {
+    return x + y + z;
+  }
 }
 
-// 第二种
-let c: Function;
-c = function (): void {
-  console.log("It work");
-};
+let n = sum(1, 2);
+console.log(n);
 
-// 不能将类型“"xxx"”分配给类型“Function”。
-// c = "xxx";
+n = sum(1, 2, 3);
+console.log(n);
 
-// 第三种, 限制参数和返回值
-let d: (para: string) => string;
-d = function (pass: string): string {
-  return pass;
-};
+// 函数名相同 参数类型不同类型
+function divide(x: number, y: number): number;
+function divide(str: string, y: number): string[];
 
-// 第四种 类型别名
-type e = (para: string) => string;
-
-const f: e = function (pass: string): string {
-  return pass;
-};
-
-// 第五种 接口
-interface g {
-  (para: string): string;
+function divide(x: any, y: number): any {
+  if (typeof x === "number") {
+    return x / y;
+  } else if (typeof x === "string") {
+    return [x.substring(0, y), x.substring(y)];
+  }
 }
 
-const h: g = (pass: string) => pass;
-const i: g = function (pass: string): string {
-  return pass;
-};
+let n1: number = divide(6, 2);
+console.log(n1);
+let s1: string[] = divide("football", 4);
+console.log(s1);
 
-// ------ 分割线 --------
+// class 重载
+// 静态方法和实例方法
+class Util {
+  static divide(x: number, y: number): number;
+  static divide(str: string, y: number): string[];
 
-function test(cb: () => string) {
-  let s = cb();
-  return s;
+  static divide(x: any, y: number): any {
+    if (typeof x === "number") {
+      return x / y;
+    } else if (typeof x === "string") {
+      return [x.substring(0, y), x.substring(y)];
+    }
+  }
 }
 
-console.log(test(() => "Hello"));
+// let a1: Util = new Util();
+// console.log(a1.divide(2, 1));
+// console.log(a1.divide("hello world", 4));
+
+let c: number = Util.divide(6, 2);
+console.log(c);
