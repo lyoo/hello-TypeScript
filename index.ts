fc603014eb21389769eb81f681061b2ce359aace
lyoo <32499368@qq.com>
2020-05-05 10:07:24 +0800

    58 generics interface indexable

--- index.ts
+++ index.ts
@@ -1,44 +1,27 @@
+// interface States<R> {
+//   [state: string]: R;
+// }
+
+// let s: States<boolean> = { enabled: true, maximized: false };
+// console.log(s);
+// console.log(s.enabled);
+
 interface Pair<F, S> {
   first: F;
   second: S;
 }
 
-let p: Pair<string, number> = { first: "lyoo", second: 45 };
-console.log(p);
-
-interface Command<T, R> {
-  id: T;
-  run(): R;
+interface States<F, S> {
+  [state: string]: Pair<F, S>;
 }
 
-let c: Command<string, number> = {
-  id: Math.random().toString(36),
-  run: function () {
-    return 3;
+let s: States<number, boolean> = {
+  enabled: { first: 1, second: true },
+  maximized: {
+    first: 2,
+    second: false,
   },
 };
 
-console.log(c.id);
-console.log(c.run());
-
-interface ElementChecker {
-  // 函数
-  <T>(items: T[], toBeChecked: T, atIndex: number): boolean;
-}
-
-function checkElementAt<T>(
-  elements: T[],
-  toBeChecked: T,
-  atIndex: number
-): boolean {
-  return elements[atIndex] === toBeChecked;
-}
-
-// 函数
-let checker: ElementChecker = checkElementAt;
-let items = [1, 3, 5, 7];
-let b: boolean = checker<number>(items, 5, 2);
-console.log("b: ", b);
-
-let d: boolean = checker<number>(items, 5, 3);
-console.log("d: ", d);
+console.log(s);
+console.log(s["enabled"]);
